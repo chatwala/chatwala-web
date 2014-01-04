12fd18bd76d1c352d0162207dd4f8019dc9a1096
rahulkumarny <rahul.kumar05@gmail.com>
2014-01-04 00:41:08 -0500

    Fixing link

--- js/redirect.js
+++ js/redirect.js
@@ -44,7 +44,7 @@ function appStoreLink() {
 	else if (isMobile.iOS() && (messageParameter && !(messageParameter === ""))) {
 		_gaq.push(['_trackEvent', 'REDIRECT', 'IOS']);	
 		setTimeout(function() {
-  			window.location = "itunes.apple.com/us/app/youtube/id544007664?mt=8";
+  			window.location = "http://itunes.apple.com/us/app/youtube/id544007664?mt=8";
 		}, 10);
 		
 		window.location = "chatwala://message/" + messageParameter;
