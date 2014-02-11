ee72c7e88825c415687590be77bbbfa4d280eb4e
Kevin Miller <Airswoop1@gmail.com>
2014-02-10 22:12:37 -0500

    added dev and qa redirects take 2

---
+++ js/redirect_qa.js
@@ -0,0 +1,56 @@
+var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
+        }
+    };
+
+function messageIdFromURL(){
+	var URL = window.location.href;
+	var res;
+	if(!(URL.indexOf('#') === -1)) {
+		res = URL.split("#");
+	}
+	else if(!(URL.indexOf('?') === -1)) {
+	  	res = URL.split("?");
+	}
+	
+	else {
+		return undefined;
+	}
+	
+	return res[res.length - 1];
+}
+
+function appStoreLink() {
+	var messageParameter = messageIdFromURL();
+	var _gaq = _gaq || [];
+	
+	if ( isMobile.Android() && (messageParameter && !(messageParameter === ""))) {
+		_gaq.push(['_trackEvent', 'REDIRECT', 'ANDROID']);
+		window.location = "http://www.chatwala.com/droidredirect.html?" + messageParameter;
+    }
+	else if (isMobile.iOS() && (messageParameter && !(messageParameter === ""))) {
+		_gaq.push(['_trackEvent', 'REDIRECT', 'IOS']);	
+		setTimeout(function() {
+  			window.location = "itms-apps://itunes.apple.com/us/app/chatwala-video-messenger/id775982711";
+		}, 50);
+		
+		window.location = "chatwala-qa://message/" + messageParameter;
+	}
+	else {} // Don't do anything here - let them sit on the homepage
+};
