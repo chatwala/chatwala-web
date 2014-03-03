cdcbc479f621a4fdc3c27b90ac604516092375e6
Kevin Miller <Airswoop1@gmail.com>
2014-03-03 18:30:01 -0500

    modified formatting

--- js/store_message_dev.js
+++ js/store_message_dev.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	if(typeof localStorage.firstMessageID !== 'undefined'){
 		var id = localStorage.firstMessageID;
 		
-		window.location = "chatwala-dev://" + id;
+		window.location = "chatwala-dev://message/" + id;
 
 	}
 	else{
